e007cf2047df1c158abf5a17db46425f355148d0
joaqfer <joaquin.fdez.ruiz@gmail.com>
2013-10-25 21:57:52 +0200

    Test ListCardNullReturnException

---
+++ KataPokerClassLibrary/Poker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataPokerClassLibrary
+{
+  public class Poker
+  {
+    public void HandVerifier(List<Card> listCard)
+    {
+      throw new ArgumentException("numero de cartas invalido");
+      
+
+    }
+  }
+
+  public class Card
+  {
+
+  }
+}
---
+++ KataPokerClassLibraryTest/PokerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KataPokerClassLibrary;
+using NUnit.Framework;
+
+namespace KataPokerClassLibraryTest
+{
+  [TestFixture]
+  class PokerTest
+  {
+    [Test]
+    public void ListCardNullReturnException()
+    {
+      var poker = new Poker();
+      var exception = Assert.Throws<ArgumentException>(() => poker.HandVerifier(null));
+      Assert.That(exception, Has.Message.EqualTo("numero de cartas invalido"));
+    }
+
+  }
+
+  
+}
