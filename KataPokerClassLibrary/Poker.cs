4f1055d7fddc8055a95fda413b80cad1c90e2087
joaqfer <joaquin.fdez.ruiz@gmail.com>
2013-10-25 22:52:02 +0200

    test ListWithTwoCardsEqualsReturnException rojo

--- KataPokerClassLibrary/Poker.cs
+++ KataPokerClassLibrary/Poker.cs
@@ -10,14 +10,52 @@ namespace KataPokerClassLibrary
   {
     public void HandVerifier(List<Card> listCard)
     {
-      throw new ArgumentException("numero de cartas invalido");
+      if(listCard==null)
+        throw new ArgumentException("lista de cartas null");
+      if (listCard.Count() != 5)
+        throw new ArgumentException("numero de cartas incorrecto");
       
 
     }
   }
 
+  public enum CardNumber
+  {
+    Ace,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    J,
+    Q,
+    K 
+  }
+
+  public enum CardColor
+  {
+    Spades,
+    Diamonds,
+    Clover,
+    Hearts 
+  }
+
   public class Card
   {
+    private readonly CardNumber _cardNumber;
+    private readonly CardColor _cardColor;
+
+    public Card(CardNumber cardNumber, CardColor cardColor)
+    {
+      this._cardNumber = cardNumber;
+      this._cardColor = cardColor;
+    }
+    public CardNumber Number { get; set; }
+    public CardColor Color { get; set; }
 
   }
 }
--- KataPokerClassLibraryTest/PokerTest.cs
+++ KataPokerClassLibraryTest/PokerTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,15 +16,29 @@ namespace KataPokerClassLibraryTest
     public void ListCardNullReturnException()
     {
       var exception = Assert.Throws<ArgumentException>(() => new Poker().HandVerifier(null));
-      Assert.That(exception, Has.Message.EqualTo("numero de cartas invalido"));
+      Assert.That(exception, Has.Message.EqualTo("lista de cartas null"));
     }
 
     [Test]
     public void ListCardDistinctFiveReturnException()
     {
-      var listCard = new List<Card>();
-      var exception = Assert.Throws<ArgumentException>(() => new Poker().HandVerifier(listCard));
-      Assert.That(exception, Has.Message.EqualTo("numero de cartas invalido"));
+      var poker = new Poker();
+      var listCard = new List<Card> {new Card(CardNumber.Ace, CardColor.Diamonds)};
+      var exception = Assert.Throws<ArgumentException>(() => poker.HandVerifier(listCard));
+      Assert.That(exception, Has.Message.EqualTo("numero de cartas incorrecto"));
+    }
+
+    [Test]
+    public void ListWithTwoCardsEqualsReturnException()
+    {
+      var poker = new Poker();
+      var listCard = new List<Card>
+        {
+          new Card(CardNumber.Ace, CardColor.Diamonds),
+          new Card(CardNumber.Ace, CardColor.Diamonds)
+        };
+      var exception = Assert.Throws<ArgumentException>(() => poker.HandVerifier(listCard));
+      Assert.That(exception, Has.Message.EqualTo("dos cartas iguales"));
     }
 
   }
